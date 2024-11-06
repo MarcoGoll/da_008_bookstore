9b236c1e13e411c2e7873c6de47e4a49dd63f6e8
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2024-11-06 07:24:28 +0100

    create: book+comment render functions

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
 /*====================================================================================================
     FUNCTIONS
 ====================================================================================================*/ 
-// add functions here
 /*
 * Description for the function
 * @param {string} <variableName> Desription for the usage of a parameter
@@ -16,6 +15,51 @@
 * @param {(number|Array)} <variableName> Desription for the usage of a parameter
 * @returns {(string|Array)} <variableName> Desription for the return variable/value
 */
+function init(){
+    renderAllBooks();
+}
+
+/*
+* Description for the function
+* @param {string} <variableName> Desription for the usage of a parameter
+* @param {number} <variableName> Desription for the usage of a parameter
+* @param {(string|Array)} <variableName> Desription for the usage of a parameter
+* @param {(number|Array)} <variableName> Desription for the usage of a parameter
+* @returns {(string|Array)} <variableName> Desription for the return variable/value
+*/
+function renderAllBooks(){
+    let content__section__BooksRef = document.getElementById('content__section__Books');
+    content__section__BooksRef.innerHTML = "";
+
+    for (let i = 0; i < books.length; i++) {
+        content__section__BooksRef.innerHTML += getBookTemplate(books[i], i);
+        renderAllBookComments(books[i], i);    
+    }    
+}
+
+/*
+* Description for the function
+* @param {string} <variableName> Desription for the usage of a parameter
+* @param {number} <variableName> Desription for the usage of a parameter
+* @param {(string|Array)} <variableName> Desription for the usage of a parameter
+* @param {(number|Array)} <variableName> Desription for the usage of a parameter
+* @returns {(string|Array)} <variableName> Desription for the return variable/value
+*/
+function renderAllBookComments(book, i){
+    let book_commentAreaRef = document.getElementById(`displayComments${i}`);
+    book_commentAreaRef.innerHTML = "";
+    if(books[i].comments.length == 0){
+        let displayCommentsRef = document.getElementById(`displayComments${i}`);
+        displayCommentsRef.innerHTML = "<i>Noch keine Kommentare vorhanden.</i>"
+    }else{
+        for (let j = 0; j < books[i].comments.length; j++) {
+            book_commentAreaRef.innerHTML += getCommentsTemplate(book, j);
+        }
+    }
+}
+
+//createMyComment
+//likeBookToggle
 
 /*====================================================================================================
     EVENT LISTENERS
---
+++ scripts/templates.js
@@ -0,0 +1,78 @@
+/*
+* Description for the function
+* @param {string} <variableName> Desription for the usage of a parameter
+* @param {number} <variableName> Desription for the usage of a parameter
+* @param {(string|Array)} <variableName> Desription for the usage of a parameter
+* @param {(number|Array)} <variableName> Desription for the usage of a parameter
+* @returns {(string|Array)} <variableName> Desription for the return variable/value
+*/
+function getBookTemplate(book, i) {
+    return `<div class="book" id="book${i}">
+                    <h2 class="book__headline">${book.name}</h2>
+                    <div class="book__dividingLine"></div>
+                    <div class="book__img">
+                        <img src="./assets/img/book.png" alt="buch">
+                    </div>
+                    <div class="book__dividingLine"></div>
+                    <div class="book__infoArea">
+                        <div class="infoArea__highlights">
+                            <div class="infoArea__price">${book.price.toFixed(2).replace(".", ",")} €</div>
+                            <div class="infoArea__likeOrNot">
+                                <div class="likes">${book.likes}</div>
+                                <svg class="heart" xmlns="http://www.w3.org/2000/svg" height="24px"
+                                    viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                                    <path
+                                        d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="infoArea__lines">
+                            <div class="infoArea__line">
+                                <p class="label">Author</p>
+                                <p class="value" id="authorValue${i}">: ${book.author}</p>
+                            </div>
+                            <div class="infoArea__line">
+                                <p class="label">Publication</p>
+                                <p class="value" id="publicationValue${i}">: ${book.publishedYear}</p>
+                            </div>
+                            <div class="infoArea__line">
+                                <p class="label">Genre</p>
+                                <p class="value" id="genreValue${i}">: ${book.genre}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="book__dividingLine"></div>
+                    <div class="book__commentArea">
+                        <h3>Comments:</h3>
+                        <div class="displayComments" id="displayComments${i}"></div>
+                        <div class="sendComment">
+                            <input type="text" name="comment" id="commentInp" placeholder="Schreibe dein Kommentar ...">
+                            <button id="sendCommentBtn"><svg xmlns="http://www.w3.org/2000/svg" height="24px"
+                                viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                                <path
+                                    d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    
+                    </div>
+                </div>`;
+}
+
+/*
+* Description for the function
+* @param {string} <variableName> Desription for the usage of a parameter
+* @param {number} <variableName> Desription for the usage of a parameter
+* @param {(string|Array)} <variableName> Desription for the usage of a parameter
+* @param {(number|Array)} <variableName> Desription for the usage of a parameter
+* @returns {(string|Array)} <variableName> Desription for the return variable/value
+*/
+function getCommentsTemplate(book, i) {
+    return `
+    <div class="displayComment">
+        <div class="submitter">[${book.comments[i].name}]</div>
+        <div class="comment">: ${book.comments[i].comment}</div>
+    </div>
+    `;
+} 
